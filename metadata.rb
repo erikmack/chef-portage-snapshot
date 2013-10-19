fcaa13eb43099ea7ec31bee0c78ee58e9a67e935
Erik Mackdanz <erikmack@gmail.com>
2013-10-19 10:26:14 -0500

    Add short description in metadata

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'portage_snapshot'
 maintainer       'Erik Mackdanz'
 maintainer_email 'erikmack@gmail.com'
 license          'Affero GPL v3'
-description      'Installs/Configures portage_snapshot'
+description      'Install a portage snapshot from a remote HTTP server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
